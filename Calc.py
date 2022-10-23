5435f88e39ee4fb92b6fa4524a9136608431cd71
Andriy <andriyshrub@gmail.com>
2022-10-23 19:57:35 +0300

    add def

--- Calc.py
+++ Calc.py
@@ -1,1 +1,40 @@
 import math
+
+def add(var1, var2):
+    return var1 + var2
+
+
+def subtract(var1, var2):
+    return var1 - var2
+
+
+def multiplication(var1, var2):
+    return var1 * var2
+
+
+def division(var1, var2):
+    return var1 / var2
+
+
+def degree(var1, var2):
+    return var1 ** var2
+
+
+def square_root(var1, var2):
+    return math.sqrt(var1), math.sqrt(var2)
+
+
+def cube_root(var1, var2):
+    return (var1**(1/3)), (var2**(1/3))
+
+
+def cosine(var1, var2):
+    return math.cos(var1), math.cos(var2)
+
+
+def sinus(var1, var2):
+    return math.sin(var1), math.sin(var2)
+
+
+def tangent(var1, var2):
+    return math.tan(var1), math.tan(var2)
